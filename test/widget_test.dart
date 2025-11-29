26aee04e3ae6b0ce811a8cf7097bea6ca49748a2
unknown <foga4590>
2025-11-29 03:59:07 +0200

    intro + splash + sign up + login design without API

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:medconnect_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App loads successfully', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
